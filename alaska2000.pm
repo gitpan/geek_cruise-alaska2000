8b65552196d900168b3cfcc77781e24a608f0e82
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2000-06-14 09:15:33 +0000

    Import of ANDK/geek_cruise-alaska2000-1.00 from CPAN.
    gitpan-cpan-distribution: geek_cruise-alaska2000
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         ANDK/geek_cruise-alaska2000-1.00.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

---
+++ alaska2000.pm
@@ -0,0 +1,16 @@
+package geek_cruise::alaska2000;
+$VERSION = "1.00";
+
+=pod
+
+This package is only contained in this distribution to have it indexed
+by the indexer, so it can be found via CPAN::Site or CPAN.pm. The
+distribution does not contain code. It contains the slides of my CPAN
+talk on the Alaska Geek Cruise 2000.
+
+Thanks to Neil Bauman and Geek Cruises Inc. for the invitation on the
+cruise and to the fellow perl geeks that helped to make the Cruise the
+success it was.
+
+=cut
+
